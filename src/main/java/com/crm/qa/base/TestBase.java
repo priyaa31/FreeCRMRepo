49907e709ffffb7211b22d6e10bc5709e6921936
priyasham <priyasham4@yahoo.com>
2018-03-16 16:21:52 +0530

    First git commit

---
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,73 @@
+package com.crm.qa.base;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	FileInputStream ip;
+	
+	public TestBase(){
+		try {
+			prop = new Properties();
+			ip = new FileInputStream("D:\\Neon\\FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		//String browserName="chrome";
+		if(browserName.equals("chrome")){
+			 System.setProperty("webdriver.chrome.driver","D:/Testing/chromedriver.exe");
+			driver = new ChromeDriver(); 
+		}
+		else if(browserName.equals("FF")){
+			 System.setProperty("webdriver.gecko.driver","D:/Testing/geckodriver.exe");
+			driver = new FirefoxDriver(); 
+		}
+		  
+		//driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+		//driver.get("https://www.freecrm.com/");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
