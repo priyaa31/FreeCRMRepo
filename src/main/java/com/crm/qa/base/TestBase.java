9173fb7de30b9822d941fb1a213f698b7ba65568
priyasham <priyasham4@yahoo.com>
2018-03-21 10:37:30 +0530

    Third Commit

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -54,7 +54,7 @@ public class TestBase {
 			driver = new FirefoxDriver(); 
 		}
 		  
-		//driver.manage().deleteAllCookies();
+		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
